e6eeb6223a597992f9b5cacae1227c5b2c3cf231
carloshenrique-santos <c.tarabal@hotmail.com>
2023-01-31 22:38:12 -0300

    named routes and handling error

--- lib/modules/not_found/not_found_page.dart
+++ lib/modules/not_found/not_found_page.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:go_router_example_test/routing/routes.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: const Center(
-        child: CircularProgressIndicator(),
+        child: Text('Not found page'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.goNamed(AppRoutes.register.name),
+        onPressed: () => context.goNamed(AppRoutes.home.name),
       ),
     );
   }
--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -1,7 +1,10 @@
 import 'package:go_router/go_router.dart';
+import 'package:go_router_example_test/modules/not_found/not_found_page.dart';
 
 import '../modules/home/home.dart';
 import '../modules/register/register.dart';
+
+enum AppRoutes { home, register }
 
 final GoRouter routes = GoRouter(
   initialLocation: '/',
@@ -9,12 +12,14 @@ final GoRouter routes = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: AppRoutes.home.name,
       builder: (context, state) {
         return const HomePage();
       },
       routes: [
         GoRoute(
           path: 'register',
+          name: AppRoutes.register.name,
           builder: (context, state) {
             return const RegisterPage();
           },
@@ -22,4 +27,7 @@ final GoRouter routes = GoRouter(
       ],
     ),
   ],
+  errorBuilder: (context, state) {
+    return const NotFoundPage();
+  },
 );
